7496ce0f5d4dbd2b6356f61fe5a7b339f6618f6a
kranonetka <fakemail4steam@ya.ru>
2017-04-14 11:57:29 +0700

    Delete HuffmanNode.cpp

--- HuffmanNode.cpp
+++
@@ -1,6 +0,0 @@
-#include "HuffmanNode.h"
-#include <cstddef>
-
-HuffmanNode::HuffmanNode(const unsigned char _symb) : byte(_symb), leftChildPtr(NULL), rightChildPtr(NULL) {};
-
-HuffmanNode::HuffmanNode(const unsigned char _symb, HuffmanNode* leftPtr, HuffmanNode* rightPtr) : byte(_symb), leftChildPtr(leftPtr), rightChildPtr(rightPtr) {};
